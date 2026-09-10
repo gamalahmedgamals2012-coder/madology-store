a7b5cc20f1a0ab02a22d69c688c45fd04625cbf8
Gamal Ahmed <gamalahmedgamals2012@gmail.com>
2026-09-10 15:28:52 +0300

    Add stable navbar language selector slots

--- public/i18n.js
+++ public/i18n.js
@@ -104,6 +104,9 @@
     }
     else if (accountTabs) { const settings = document.createElement("div"); settings.className = "mado-language-settings"; settings.innerHTML = '<span>Settings</span>'; settings.append(item); accountTabs.append(settings); }
     else if (document.querySelector("#dropdownMenu")) { const settings = document.createElement("div"); settings.className = "mado-language-settings"; settings.innerHTML = '<span>Settings</span>'; settings.append(item); document.querySelector("#dropdownMenu").append(settings); }
+    else if (document.querySelector(".mado-language-desktop-slot")) {
+      document.querySelector(".mado-language-desktop-slot").append(item);
+    }
     else if (document.querySelector(".navbar-actions")) {
       const actions = document.querySelector(".navbar-actions");
       const toggler = actions.querySelector(".navbar-toggler");
